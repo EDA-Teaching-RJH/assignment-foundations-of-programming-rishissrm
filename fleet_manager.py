2031a6a9b8e0d4d7bcea2dde7de8196eb1f9a08c
rishissrm <ssrm@kent.ac.uk>
2026-02-14 23:36:12 +0000

    display menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -9,3 +9,22 @@ def init_database():
     IDs =["101" , "102", "103", "104","105"]
     return Names,Ranks,Divisions,IDs
 
+# display menu options
+def display_menu():
+    user = input("Enter your full name : ")
+    print("Hello current student logged in  "+ user)
+    print("*******Menu***********")
+    print("1.Display Roaster")
+    print("2.Add Member")
+    print("3.Remove Memeber")
+    print("4.Update Rank")
+    print("5.Search Crew")
+    print("6.Filter By Division")
+    print("7.Calculate Payroll")
+    print("8.Count Officers")
+    print("9.Exit")
+
+    choice = input("Choose an option: ")
+
+display_menu()
+
